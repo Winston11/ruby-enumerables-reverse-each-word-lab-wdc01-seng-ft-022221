acfe350a929b3f5bd2b91d5b0c01612d3d102089
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-19 18:43:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
 def reverse_each_word(word)
   
 reverse_array = []
-array_of_string = word.split("")
+array_of_string = word.split(" ")
 revers = ""
 
 reverse_array = array_of_string.collective{ |value| value.reverse }
 
 
-reverse = reverse_array.join("")
+reverse = reverse_array.join(" ")
 
 return reverse 
 
